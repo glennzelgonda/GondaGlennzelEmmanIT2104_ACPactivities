2c4d479cdaa89754c33eaedc0472edf7f7455919
glennzelgonda <23-03791@g.batstate-u.edu.ph>
2024-08-28 13:04:24 +0800

    Add files via upload

---
+++ Exercise Set 01/main.py
@@ -0,0 +1,6 @@
+print ("Welcome to CS 121: Advance  Computer Programming!\n") 
+
+str1 = input ("Enter your name: ")
+str2 = f"\nThis course will be fun, {str1}!"
+concatenated_str = str1 + " " + str2
+print(concatenated_str)
