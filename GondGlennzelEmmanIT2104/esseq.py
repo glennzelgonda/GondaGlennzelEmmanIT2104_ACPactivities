1bb70e3fbf45cfaf3dcca4fe5773f2563a7e22e6
glennzelgonda <23-03791@g.batstate-u.edu.ph>
2024-08-28 13:05:47 +0800

    Delete GondGlennzelEmmanIT2104 directory

--- GondGlennzelEmmanIT2104/esseq.py
+++
@@ -1,5 +0,0 @@
-for num in range(7, 42, 7):
-    if num % 2 == 0:
-        print (f"\t{num}")
-    else:
-        print (num)
--- GondGlennzelEmmanIT2104/tempconvert.py
+++
@@ -1,3 +0,0 @@
-C = float(input("Input Celcius Value: "))
-F = C * 9 / 5 + 32
-print("\n{:.2f} Celcius is {:.2f} Fahrenheit".format( C, F))
