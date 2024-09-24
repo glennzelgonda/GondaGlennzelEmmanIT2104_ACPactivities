abedda825cdad8cb78d00515ff99fc28801e763a
glennzelgonda <23-03791@g.batstate-u.edu.ph>
2024-09-24 08:13:11 +0800

    Delete Exercise  Set 02 directory

--- Exercise  Set 02/bitwise2.py
+++
@@ -1,3 +0,0 @@
-integer = int(input("Enter an integer: "))
-value = integer & 30
-print (f"{integer} AND 30 =  {value}")
--- Exercise  Set 02/typecasting.py
+++
@@ -1,4 +0,0 @@
-i = int(input("Enter an integer: "))
-f = float(input("Enter a float: "))
-difference = i - f
-print("Difference: {:.2f}".format(difference))
